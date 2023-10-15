c85b0e6316981a9a3723a8e26ac8aa7e1f61c5af
AnyaRatusnyak <anyaaboyko@gmail.com>
2023-10-15 15:34:36 +0300

    changing players

--- tic-tac-toe/src/Board.jsx
+++ tic-tac-toe/src/Board.jsx
@@ -3,34 +3,44 @@ import Square from "./Square";
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
-  const handleClick = () => {
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const handleClick = (i) => {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[0] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
 
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   };
   return (
     <div>
       <div>
-        <Square value={squares} onSquareClick={handleClick} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
 
-        <Square value={squares} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
 
-        <Square value={squares} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div>
-        <Square value={squares} onSquareClick={handleClick} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
 
-        <Square value={squares} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
 
-        <Square value={squares} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div>
-        <Square value={squares} onSquareClick={handleClick} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
 
-        <Square value={squares} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
 
-        <Square value={squares} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </div>
   );
