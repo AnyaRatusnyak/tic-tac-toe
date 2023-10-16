95bf71b0f25859e2d442fa4c8f0160cb1c57f353
AnyaRatusnyak <anyaaboyko@gmail.com>
2023-10-16 10:51:17 +0300

    added winner

--- tic-tac-toe/src/Board.jsx
+++ tic-tac-toe/src/Board.jsx
@@ -6,7 +6,7 @@ const Board = () => {
   const [xIsNext, setXIsNext] = useState(true);
 
   const handleClick = (i) => {
-    if (squares[i]) {
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
     const nextSquares = squares.slice();
@@ -19,8 +19,37 @@ const Board = () => {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   };
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+  const winner = calculateWinner(squares);
   return (
     <div>
+      <div>
+        {winner
+          ? "Winner is " + winner
+          : "Next player is : " + (xIsNext ? "X" : "O")}
+      </div>
       <div>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
 
